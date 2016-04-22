2823b247a0d7fb65ab4983f109121766c8b87132
Skaiiwalker <lbar8299@740s-pl-369.local>
2016-04-22 09:52:11 -0600

    Shapepanel

--- src/ctec/view/ShapePanel.java
+++ src/ctec/view/ShapePanel.java
@@ -1,6 +1,29 @@
 package ctec.view;
 
-public class ShapePanel 
+import javax.swing.JPanel;
+import java.awt.Rectangle;
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+public class ShapePanel extends JPanel
 {
+	private ArrayList<Rectangle> rectangleList;
+	//more arraylists here
 	
+	public ShapePanel()
+	{
+		rectangleList = new ArrayList<Rectangle>();
+	}
+	
+	public void addRectangle()
+	{
+		//random info here
+		rectangleList.add(new Rectangle(10,20,50,10));
+	}
+	
+	@Override
+	protected void paintComponent(Graphics currentGraphics)
+	{
+		//draw in here!
+	}
 }
