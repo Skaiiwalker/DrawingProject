7bde231b442f09f0c8ac9bc5bc2ee1568dfcee99
Skaiiwalker <lbar8299@740s-pl-369.local>
2016-05-27 09:57:06 -0600

    Even more

---
+++ src/ctec/controller/DrawingController.java
@@ -0,0 +1,22 @@
+package ctec.controller;
+
+import ctec.view.*;
+/**
+ * @author Luke Barrowes
+ *@version 0. x May 27, 2016
+ */
+
+public class DrawingController 
+{
+	private DrawingFrame baseFrame;
+	
+	public DrawingController()
+	{
+		baseFrame = new DrawingFrame(this);
+	}
+	
+	public void start()
+	{
+		
+	}
+}
